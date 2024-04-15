82043843a9d2ba864dd1a51e67a9b7efaf47460a
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-04-15 18:30:42 +0200

    Add OrbitControls for camera rotation

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/Addons.js'
 
 const canvas = document.querySelector('canvas.webgl')
 
@@ -10,17 +11,21 @@ const sizes = {
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 1000);
 camera.position.z = 5;
-const renderer = new THREE.WebGLRenderer({ canvas : canvas });
+const renderer = new THREE.WebGLRenderer({ canvas : canvas, antialias: true });
 renderer.setSize(sizes.width, sizes.height)
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({color: 0xfffff00});
+const geometry = new THREE.IcosahedronGeometry(1, 8);
+const loader = new THREE.TextureLoader();
+const material = new THREE.MeshStandardMaterial({
+    map: loader.load("./earthmap1k.jpg")
+});
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube);
 
-const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+const light = new THREE.HemisphereLight();
 scene.add( light );
 
+new OrbitControls(camera, canvas)
 
 window.addEventListener('rezise', () => {
 
@@ -37,8 +42,8 @@ window.addEventListener('rezise', () => {
 function animate(){
     window.requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.02;
+    cube.rotation.x += 0.001;
+    cube.rotation.y += 0.002;
 
     renderer.render(scene, camera)
 }
